ea8494ed4dee38db22bf90cc899bbe8772c06123
Slawomir Leski <slawomir.leski@gmail.com>
2018-09-12 12:29:55 +0200

    changing_actors_scheduler

--- app/akka/HelloActor.java
+++ app/akka/HelloActor.java
@@ -1,7 +1,6 @@
 package akka;
 
 import akka.actor.AbstractLoggingActor;
-import play.Logger;
 
 import javax.inject.Inject;
 
@@ -10,29 +9,16 @@ import javax.inject.Inject;
  */
 public class HelloActor extends AbstractLoggingActor {
 
-	private static final Logger.ALogger LOG = Logger.of(HelloActor.class);
-
 	public final static String NAME = "HELLO_ACTOR";
 
 	@Inject
 	public HelloActor() {
-		LOG.info("-----------------------------------");
-		LOG.info(NAME + " was created");
 
-		receiveBuilder().match(String.class, s -> {
-			log().info("Received String message: {}", s);
-		}).matchAny(o -> log().info("received unknown message")).build();
 	}
-
-
 
 	@Override
 	public Receive createReceive() {
-		LOG.info("--------createReceive()-------");
-		log().info("createReceive ----------------------------------------");
-		return receiveBuilder().match(String.class, s -> {
-			log().info("Received String message: {}", s);
-		}).matchAny(o -> log().info("received unknown message")).build();
+		return receiveBuilder().match(String.class, s -> log().info("Received String message: {}", s)).matchAny(o -> log().info("received unknown message")).build();
 	}
 
 }
--- app/modules/QuartzSchedulerHelper.java
+++ app/modules/QuartzSchedulerHelper.java
@@ -3,6 +3,7 @@ package modules;
 import akka.HelloActor;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
 import com.typesafe.akka.extension.quartz.QuartzSchedulerExtension;
 import org.apache.commons.lang3.RandomStringUtils;
 import play.Logger;
@@ -10,9 +11,7 @@ import play.api.Configuration;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import java.util.Arrays;
 import java.util.Date;
-import java.util.List;
 
 /**
  * Created by Slawomir Leski on 05-07-2018.
@@ -25,16 +24,13 @@ public class QuartzSchedulerHelper {
 	@Inject
 	public QuartzSchedulerHelper(ActorSystem registry, QuartzSchedulerExtension quartzSchedulerExtension, Configuration configuration) {
 
-		List<String> actors = Arrays.asList(HelloActor.NAME);
-		for (String name : actors) {
-			ActorRef actor = registry.actorFor(name);
-			schedule(quartzSchedulerExtension, actor, name, RandomStringUtils.randomAlphabetic(10));
-		}
+		ActorRef helloActor = registry.actorOf(Props.create(HelloActor.class), "hello-actor");
+		schedule(quartzSchedulerExtension, helloActor, HelloActor.NAME, RandomStringUtils.randomAlphabetic(10));
 
 	}
 
 	private void schedule(QuartzSchedulerExtension quartzExtension, ActorRef actor, String actorName, String message) {
-		Date schedule = quartzExtension.schedule(actorName, actor, message);
+		Date schedule = quartzExtension.schedule(actorName, actor,  message);
 		LOG.info("Job {} scheduled at {}.", actorName, schedule);
 	}
 }
